66a4acb6eaa524c361d4c54fe236bd5f76cf1112
ephkk <68995803+ephkk@users.noreply.github.com>
2025-09-06 10:34:07 +0000

    fix: update leaderboard sorting logic and refactor activity, team, and user components

--- octofit-tracker/frontend/src/components/Users.js
+++ octofit-tracker/frontend/src/components/Users.js
@@ -45,33 +45,3 @@ const Users = () => {
 };
 
 export default Users;
-import React, { useEffect, useState } from 'react';
-
-const API_URL = `https://${process.env.REACT_APP_CODESPACE_NAME}-8000.app.github.dev/api/users/`;
-
-function Users() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch(API_URL)
-      .then(res => res.json())
-      .then(data => {
-        console.log('Users API endpoint:', API_URL);
-        console.log('Fetched users:', data);
-        setUsers(data.results || data);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h2>Users</h2>
-      <ul>
-        {users.map((user, idx) => (
-          <li key={idx}>{user.name} ({user.email}) - Team: {user.team}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export default Users;
